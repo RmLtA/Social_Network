1fdb6246890f4507c2a9bbeb81e2eab531b6fa9b
RmLtA <rm.liantsoa@gmail.com>
2015-04-21 18:04:05 +0200

    little changes

--- SNAP/circle.py
+++
@@ -1,9 +0,0 @@
-import snap
-
-Graph = snap.GenCircle(snap.PNEANet, 3, 2)
-for EI in Graph.Edges():
-    print "edge: (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
-	
-#generating the png graph	
-snap.DrawGViz(Graph, snap.gvlDot, "graph.png", "graph 1")
-print "Graph Viz generated ... "
